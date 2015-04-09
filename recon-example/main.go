a9efa468c4d1e4c46a7d96e059d2b8c69ec8dba8
Jimmy Sawczuk <github@jimmysawczuk.com>
2015-04-09 09:38:00 -0400

    Restructuring into a package

--- recon-example/main.go
+++ recon-example/main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"recon"
 )
 
 func main() {
 	url := os.Args[1]
 
-	parser := NewParser()
+	parser := recon.NewParser()
 	res, _ := parser.Parse(url)
 
 	jres, _ := json.MarshalIndent(res, "", "   ")
