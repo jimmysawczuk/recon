3a9389649ab8b51acd761a73c25a6e9f81468092
Jimmy Sawczuk <me@jimmysawczuk.com>
2015-09-23 16:02:49 -0400

    Fixing an issue for when an img retrieval returns no response.

--- recon.go
+++ recon.go
@@ -332,7 +332,10 @@ func (p *Parser) analyzeImages() []Image {
 		go func(img_url *url.URL, tag imgTag) {
 			req, _ := http.NewRequest("GET", img_url.String(), nil)
 			req.Header.Add("User-Agent", "recon (github.com/jimmysawczuk/recon; similar to Facebot, facebookexternalhit/1.1)")
-			resp, _ := p.client.Do(req)
+			resp, err := p.client.Do(req)
+			if err != nil {
+				return
+			}
 
 			img := image{
 				url:          img_url.String(),
